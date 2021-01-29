cece137658d23923609ba1ae12fa374f5d98a06b
Dane Murphy <danem7@gmail.com>
2021-01-28 23:36:23 -0800

    only handling border bottom

--- frontend/src/components/main/styled-components/Border.jsx
+++ frontend/src/components/main/styled-components/Border.jsx
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 export default styled.div`
-    border-top: 1px solid rgb(221, 221, 221);
-    border-bottom: 1px solid rgb(221, 221, 221);
-
     width: 100%;
     max-width: 1128px;
+    border-bottom: 1px solid rgb(221, 221, 221);
 `;
