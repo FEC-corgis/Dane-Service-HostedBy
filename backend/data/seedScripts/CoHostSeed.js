0d8c28df25f88d0e8a7c575593a972b95ba80d8c
Dane Murphy <danem7@gmail.com>
2021-02-03 01:34:07 -0800

    cohosts added

--- backend/data/seedScripts/CoHostSeed.js
+++ backend/data/seedScripts/CoHostSeed.js
@@ -5,7 +5,7 @@ module.exports = (async function () {
     for (let i = 0; i < 25; i++) {
         await CoHost.create({
             HostId: 101 + Math.floor(Math.random() * 25),
-            HostedById: Math.floor(Math.random() * 100),
+            HostedById: i === 0 ? 1 : i,
         });
     }
 })();
