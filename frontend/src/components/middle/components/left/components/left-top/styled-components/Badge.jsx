3ae53c32e7dd9b4e7b6cdae8b44aacb2d51f3f15
Dane Murphy <danem7@gmail.com>
2021-02-03 20:04:36 -0800

    added margin to suphost badge for responsive centering of badges'

--- frontend/src/components/middle/components/left/components/left-top/styled-components/Badge.jsx
+++ frontend/src/components/middle/components/left/components/left-top/styled-components/Badge.jsx
@@ -5,4 +5,8 @@ export default styled(Flex)`
     align-items: center;
     margin-bottom: 24px;
     padding: 0 12px;
+
+    @media (max-width: 1050px) {
+        margin-left: ${(props) => props.move && '5px'};
+    }
 `;
