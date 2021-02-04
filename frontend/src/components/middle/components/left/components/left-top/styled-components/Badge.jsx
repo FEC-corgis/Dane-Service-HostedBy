97b45be4f4c1f9f2f93c41f6f68d1336f78a924e
Dane Murphy <danem7@gmail.com>
2021-02-03 20:29:35 -0800

    adjust media query

--- frontend/src/components/middle/components/left/components/left-top/styled-components/Badge.jsx
+++ frontend/src/components/middle/components/left/components/left-top/styled-components/Badge.jsx
@@ -6,7 +6,7 @@ export default styled(Flex)`
     margin-bottom: 24px;
     padding: 0 12px;
 
-    @media (max-width: 1050px) {
+    @media (max-width: 1064px) {
         margin-left: ${(props) => props.move && '5px'};
     }
 `;
